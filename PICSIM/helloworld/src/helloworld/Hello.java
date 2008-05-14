bbfd96e2c039702951206812b649506d15c0e77c
JoergMogielinski <JoergMogielinski@35860f08-f44b-0410-bcbf-13333a9b3490>
2008-05-14 12:51:39 +0000

    Erste Programmversion. Ein einfaches Hello World Programm in Java zur Demonstration des SVN-Plugin für Netbeans.

---
+++ PICSIM/helloworld/src/helloworld/Hello.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class Hello
+{
+    public String hello;
+    
+    Hello()
+    {    
+        this.hello = "Hello World";
+    }
+    
+    public String getHelloMessage()
+    {
+        return this.hello;
+    }
+}
---
+++ PICSIM/helloworld/src/helloworld/Main.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class Main
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Hello hw = new Hello();
+        System.out.print(hw.getHelloMessage());
+    }
+}
